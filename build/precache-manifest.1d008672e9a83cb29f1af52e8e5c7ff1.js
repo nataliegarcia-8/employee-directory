884193a264cc29a66839c65d6a159867c8306e16
Natalie Garcia <gnatalie.booking@gmail.com>
2020-10-22 22:16:25 -0400

    fixed git problems, recloned repo

---
+++ build/precache-manifest.1d008672e9a83cb29f1af52e8e5c7ff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dee699ff68b6ff67df287ebe1f5810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8274f5784119586c88d",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "877fc4c04003d61ebc78",
+    "url": "/static/css/main.d97a33e2.chunk.css"
+  },
+  {
+    "revision": "e8274f5784119586c88d",
+    "url": "/static/js/2.ddf63b60.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/static/js/2.ddf63b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877fc4c04003d61ebc78",
+    "url": "/static/js/main.0bc293bb.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
